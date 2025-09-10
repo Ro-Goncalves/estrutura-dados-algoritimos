445a02464dae5043a486375a6779896860aebab9
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-09-10 07:28:39 -0300

    Estrutura de Dados - Vetor - Método Adicionar - Testes

--- src/test/java/br/com/rgbrainlabs/estruturadados/vetor/VetorTest.java
+++ src/test/java/br/com/rgbrainlabs/estruturadados/vetor/VetorTest.java
@@ -1,6 +1,7 @@
 package br.com.rgbrainlabs.estruturadados.vetor;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,23 @@ public class VetorTest {
     void deveriaCriarVetor() {
         assertDoesNotThrow(() -> {
             new Vetor(10);
+        });
+    }
+
+    @Test
+    void deveriaAdicionarElemento() {
+        var vetor = new Vetor(1);
+        assertDoesNotThrow(() -> {
+            vetor.adicionar("elemento");
+        });
+    }
+
+    @Test
+    void quandoAdicionarMaisElementosDoQueCapacidade_deveriaLancarExcecao() {
+        var vetor = new Vetor(1);
+        vetor.adicionar("elemento");
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            vetor.adicionar("elemento");
         });
     }
 }
