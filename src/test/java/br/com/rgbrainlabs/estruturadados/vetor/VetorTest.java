beaac2150232b0e9702a2bb3ebca6ced63ca6a1d
rodrigo.goncalves <ro.go.calves@gmail.com>
2025-09-10 08:37:42 -0300

    Estrutura de Dados - Vetor - Métodos Tamanho e ToString - Testes

--- src/test/java/br/com/rgbrainlabs/estruturadados/vetor/VetorTest.java
+++ src/test/java/br/com/rgbrainlabs/estruturadados/vetor/VetorTest.java
@@ -1,6 +1,7 @@
 package br.com.rgbrainlabs.estruturadados.vetor;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,10 @@ public class VetorTest {
 
     @Test
     void deveriaAdicionarElemento() {
+        // Given
         var vetor = new Vetor(1);
+
+        // When & Then
         assertDoesNotThrow(() -> {
             vetor.adicionar("elemento");
         });
@@ -24,10 +28,41 @@ public class VetorTest {
 
     @Test
     void quandoAdicionarMaisElementosDoQueCapacidade_deveriaLancarExcecao() {
+        // Given
         var vetor = new Vetor(1);
         vetor.adicionar("elemento");
+
+        // When & Then
         assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
             vetor.adicionar("elemento");
         });
+    }
+
+    @Test
+    void deveriaRetornarTamanho() {
+        // Given
+        var vetor = new Vetor(3);
+        vetor.adicionar("elemento 1");
+        vetor.adicionar("elemento 2");
+
+        // When
+        var retorno = vetor.tamanho();
+        
+        // Then
+        assertEquals(2, retorno);
+    }
+
+    @Test
+    void deveriaRetornarToString() {
+        // Given
+        var vetor = new Vetor(3);
+        vetor.adicionar("elemento 1");
+        vetor.adicionar("elemento 2");
+        
+        // When
+        var retorno = vetor.toString();
+
+        // Then
+        assertEquals("[elemento 1, elemento 2]", retorno);
     }
 }
